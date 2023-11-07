14b77d7c1f254018763721881cd0eac3fd0ebca2
ihebbs1 <iheb.slimen@hotmail.com>
2023-11-07 18:07:59 +0100

    commit

---
+++ WebFundamentals/Week 2/Day 1/Core/script.js
@@ -0,0 +1,12 @@
+function change(element) {
+    element.innerText = "logout"
+}
+
+function deleted() {
+    var cont = document.querySelector(".but3")
+    cont.remove()
+
+}
+function message(element) {
+    alert("Ninja was liked when clicking a like button")
+}
