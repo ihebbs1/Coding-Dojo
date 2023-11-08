3b4a44162814aedd4454a548ff99a39025c9c60f
ihebbs1 <iheb.slimen@hotmail.com>
2023-11-08 12:08:58 +0100

    commit

--- WebFundamentals/Week 2/Day 1/Core/likes 1/script.js
+++
@@ -1,8 +0,0 @@
-var ellement=document.querySelector("#plus");
-var count=0;
-function likes () {
-   ellement.innerText=count+"like";
-    count++;
-    
-    
-} 
---
+++ WebFundamentals/Week 2/Day 2/Core/likes/likes 1/script.js
@@ -0,0 +1,8 @@
+var ellement = document.querySelector("#plus");
+var count = 0;
+function likes() {
+    ellement.innerText = count + "like";
+    count++;
+
+
+}   
