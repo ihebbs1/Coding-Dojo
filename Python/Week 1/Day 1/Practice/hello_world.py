29384776a475ef9c1254f85874b617127783c8e4
ihebbs1 <iheb.slimen@hotmail.com>
2023-11-13 23:42:41 +0100

    commit

---
+++ Python/Week 1/Day 1/Practice/hello_world.py
@@ -0,0 +1,22 @@
+print("hello world")
+
+
+name= "iheb"
+print("hello" , name)
+
+
+name="iheb"
+print("hello " + name)
+
+
+num= 7 
+print("hello" , num)
+
+num=7
+print("hello " + str(num))
+
+
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print( "i love to eat{fave_food1} and {fave_food2}" .format(fave_food1="sushi",fave_food2="pizza"))
+print( f"i love to eat{fave_food1} and {fave_food2}")
