2ce84f8775740b9d2fb18af88bf2c7c5b6d8c73d
ihebbs1 <iheb.slimen@hotmail.com>
2023-11-19 22:35:33 +0100

    understanding routing

---
+++ Python/Week 1/Day 1/Core/Nested Dictionaries&lists.py
@@ -0,0 +1,21 @@
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+x[1][0]=15 #ex1
+
+for student in students:
+    if student['last_name'] == 'Jordan':
+        student['last_name'] = 'Bryant'
+
+print(students)
+
+sports_directory['soccer'][sports_directory['soccer'].index('Messi')] = 'Andres'
+
+print(sports_directory)
